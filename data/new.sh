79afd8c6d54532286fbf1f4e8f066f4848df389f
Darlon <darlonv@gmail.com>
2023-08-16 11:39:06 +0000

    updates

--- data/new.sh
+++ data/new.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+#######
+## New project
+#######
 echo Creating new project: ${PROJECT}
 django-admin startproject ${PROJECT}
-echo Creating new app: ${APP}
+
+#######
+## New App
+#######
+#echo Creating new app: ${APP}
+cd ${PROJECT}
+pwd
 django-admin startapp ${APP}
